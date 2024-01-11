a51d32d8adf6c8b7b56576e47340e45c9e2bd25a
Geoffrey Challen <geoffrey.challen@gmail.com>
2024-01-10 20:43:08 -0600

    Updates

--- _next/static/SFe0l0j7cu-R5V1YDSnnM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-2e2ad703ef185ba7.js"],"/_error":["static/chunks/pages/_error-1e8943d2dc8c29b4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
